6efb30ff877fa17abb4d8eee339f780ab2acb04f
thecoder714 <75258277+thecoder714@users.noreply.github.com>
2022-03-11 22:14:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//placeholder
+camera = document.getElementById("livefeed");
+
+Webcam.set({
+    height: 350,
+    width: 500,
+    img_format: "png",
+    png_quality: 1000
+});
+
+Webcam.attach(camera);
+
+function captureImage() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById('capturedimage').innerHTML = "<img src=" + data_uri + ">"
+    });
+}
